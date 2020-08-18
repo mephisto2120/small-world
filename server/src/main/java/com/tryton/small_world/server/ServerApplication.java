6559e94cdbbb6e870784a94684ee56034a79e435
mephisto <mephisto2120@gmail.com>
2020-08-18 21:17:22 +0200

    Fixing package name

--- server/src/main/java/com/tryton/small_world/server/ServerApplication.java
+++ server/src/main/java/com/tryton/small_world/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.tryton.smallworld.server;
+package com.tryton.small_world.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- server/src/test/java/com/tryton/small_world/server/ServerApplicationTests.java
+++ server/src/test/java/com/tryton/small_world/server/ServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tryton.smallworld.server;
+package com.tryton.small_world.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
