dce311f6348683a3ca1e7348fde1ea8a48d417e3
mephisto <mephisto2120@gmail.com>
2020-09-05 12:05:22 +0200

    RF# Reconfiguring swagger

--- server/src/main/java/com/tryton/small_world/server/config/SwaggerConfig.java
+++ server/src/main/java/com/tryton/small_world/server/config/SwaggerConfig.java
@@ -6,17 +6,16 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2
-public class SpringFoxConfig {
+@EnableSwagger2WebMvc
+public class SwaggerConfig {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+        return new Docket(DocumentationType.SWAGGER_2).select()
+            .apis(RequestHandlerSelectors.basePackage("com.tryton.small_world.server.controller"))
+            .paths(PathSelectors.any())
+            .build();
     }
 }
